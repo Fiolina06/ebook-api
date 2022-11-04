ebf0a0bd8a46cda8f1859335d865a4e88843c781
Fiolina06 <fiolinaa2@gmail.com>
2022-11-04 19:27:45 +0700

    Authentifikasi

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\heloController;
 use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +19,22 @@ use App\Http\Controllers\BookController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+ });
 
-// Route::get('halo', function(){
-//     return["me" => "cantik"];
-// });
+//public route 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::resource('helo', heloController::class);
-Route::resource('siswa', SiswaController::class);
-Route::resource('book', BookController::class);
+Route::get('/books', [BookController::class,'index']);
+Route::get('/books/{id}', [BookController::class,'show']);
+Route::get('/Authors', [AuthorController::class,'index']);
+Route::get('/Authors/{id}', [AuthorController::class,'show']);
+
+//protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('books', BookController::class)->except('create','edit','show','index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('authors', AuthorController::class)->except('create','edit','show','index');;
+});
